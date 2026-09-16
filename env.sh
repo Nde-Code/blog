0077e89eac474183f4e311816871b98bce7583e4
Nathan D <84632287+Nde-Code@users.noreply.github.com>
2026-09-16 20:34:36 +0200

    chore: format some code files and append colons to .gitignore comments.

--- env.sh
+++ env.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# Code formatted with https://www.codeclean.net/tools/bash/ using 4-space indentation.
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-NC='\033[0m'
+GREEN='33[0;32m'
+RED='33[0;31m'
+BLUE='33[0;34m'
+NC='33[0m'
 
 echo -e "${BLUE}Hugo blog environment check: ${NC}"
 
@@ -11,7 +12,7 @@ check_command() {
     local cmd=$1
     local description=$2
 
-    if command -v "$cmd" &> /dev/null; then
+    if command -v "$cmd" &>/dev/null; then
 
         if [ "$cmd" = "go" ]; then
             local version=$(go version 2>&1)
@@ -27,12 +28,12 @@ check_command() {
 check_command "git" "Git Version Control"
 check_command "go" "Go Language (required for Hugo modules)"
 
-if command -v hugo &> /dev/null; then
+if command -v hugo &>/dev/null; then
     HUGO_VERSION=$(hugo version)
     if [[ $HUGO_VERSION == *"extended"* ]]; then
         echo -e "[${GREEN}OK${NC}] Hugo Static Site Generator (EXTENDED version detected). ($HUGO_VERSION)"
     else
-        YEL='\033[0;33m'
+        YEL='33[0;33m'
         echo -e "[${YEL}WARNING${NC}] Hugo is installed but it is NOT the 'extended' version. Your theme might fail to compile Sass/SCSS files."
     fi
 else
